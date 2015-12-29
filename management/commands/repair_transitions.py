e0cedce41a10f1a72fe5d620a0d0afc3084c8afd
Lumi Pakkanen <frostburn@suomi24.fi>
2015-12-29 10:31:51 +0200

    Add paging to repair_transitions management command.

--- management/commands/repair_transitions.py
+++ management/commands/repair_transitions.py
@@ -11,17 +11,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         total = Position.objects.all().count()
-        for i, position in enumerate(Position.objects.all()):
-            if i % 100 == 0:
-                self.stdout.write("%d / %d" % (i, total))
-            board = code_to_board(position.states.all().first().code)
-            for coord, child in board.children(False):
-                child_code = board_to_code(child)
-                child_state = State.objects.filter(code=child_code).first()
-                if child_state:
-                    transition, created = Transition.objects.get_or_create(source=position, target=child_state.position)
-                    if created:
-                        self.stdout.write('Transition created between')
-                        self.stdout.write(board.render())
-                        self.stdout.write('and')
-                        self.stdout.write(child.render())
+        self.stdout.write("Total positions %d" % (total,))
+        PAGE_LEN = 1000
+        max_page = 1 + total // PAGE_LEN
+        page = 0
+        while True:
+            self.stdout.write("Page %d / %d" % (page, max_page))
+            qs = Position.objects.all()[page * PAGE_LEN:(page + 1) * PAGE_LEN]
+            page += 1
+            if not qs:
+                break
+            for position in qs:
+                board = code_to_board(position.states.all().first().code)
+                for coord, child in board.children(False):
+                    child_code = board_to_code(child)
+                    child_state = State.objects.filter(code=child_code).first()
+                    if child_state:
+                        transition, created = Transition.objects.get_or_create(source=position, target=child_state.position)
+                        if created:
+                            self.stdout.write('Transition created between')
+                            self.stdout.write(board.render())
+                            self.stdout.write('and')
+                            self.stdout.write(child.render())
